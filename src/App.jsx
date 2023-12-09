aaf82737652e472451cb4deee54d884ce65a9fc7
Zaïnab <zeinab0695@gmail.com>
2023-12-09 20:25:08 +0100

    Dossier Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Login from './components/login/login'
-import Home from './Home'
-import './App.css'
-import Register from './components/formulaire/register'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Home from "./Home";
+import "./App.css";
+import Register from "./components/Register/Register";
 
 function App() {
-
   const router = createBrowserRouter([
     {
       path: "/",
@@ -21,16 +20,13 @@ function App() {
     //   path: "/Xbox",
     //   element: <Xbox />,
     // },
-
   ]);
 
   return (
     <>
-    <Login></Login>
+      <Login></Login>
       <RouterProvider router={router} />
-
-      {Register()}
-
+      <Register></Register>
     </>
   );
 }
