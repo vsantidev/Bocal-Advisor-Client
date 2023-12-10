78d0dcc105888dae63fa97f4f8e6f10a8d9129e1
Zaïnab <zeinab0695@gmail.com>
2023-12-10 22:50:56 +0100

    h3

--- src/components/home/Places.jsx
+++ src/components/home/Places.jsx
@@ -2,13 +2,13 @@ function Places(props) {
   return (
     <div className="container">
       <h2>{props.title}</h2>
-      <p>{props.street}</p>
-      <p>{props.postcode}</p>
-      <p>{props.city}</p>
+      <h4>{props.street}</h4>
+      <h4>{props.postcode}</h4>
+      <h4>{props.city}</h4>
       {/* Image ne s'affiche pas :( */}
       <img src={props.file} alt="" />
-      <p>{props.description}</p>
-      <p>{props.category_id}</p>
+      <h4>{props.description}</h4>
+      <h4>{props.category_id}</h4>
     </div>
   );
 }
