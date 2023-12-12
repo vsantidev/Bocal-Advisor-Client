792bab9fac4f95886133b55503e259e3576b678b
Zaïnab <zeinab0695@gmail.com>
2023-12-12 15:12:06 +0100

    image

--- src/components/home/CreatePLaces.jsx
+++ src/components/home/CreatePLaces.jsx
@@ -30,7 +30,7 @@ function CreatePlaces() {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/post", options);
+      const response = await fetch("http://127.0.0.1:8000/api/place", options);
       if (!response.ok) {
         alert(`HTTP error! Status: ${response.status}`);
         throw new Error(`HTTP error! Status: ${response.status}`);
