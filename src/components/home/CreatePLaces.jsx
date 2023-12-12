64e27d84bfed2563db04d6c3633b504cf4fc7102
Zaïnab <zeinab0695@gmail.com>
2023-12-12 10:24:16 +0100

    Création de lieux : fonctionnelle

--- src/components/home/CreatePLaces.jsx
+++ src/components/home/CreatePLaces.jsx
@@ -32,6 +32,7 @@ function CreatePlaces() {
     try {
       const response = await fetch("http://127.0.0.1:8000/api/post", options);
       if (!response.ok) {
+        alert(`HTTP error! Status: ${response.status}`);
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       const data = await response.json();
