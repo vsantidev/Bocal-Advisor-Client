3b932eace6ae4ffc138b7dfa544758a537a26f63
Zaïnab <zeinab0695@gmail.com>
2023-12-13 16:54:19 +0100

    show details

--- src/components/Places/Places.jsx
+++ src/components/Places/Places.jsx
@@ -5,10 +5,7 @@ function Places(props) {
       <h4>{props.city}</h4>
       {/* Image ne s'affiche pas :( */}
       <img src={props.file} />
-
-      <h4>{props.description}</h4>
       <h4>{props.name_category}</h4>
-
     </div>
   );
 }
