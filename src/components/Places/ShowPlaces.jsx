a5bdfb0bdc45e92b5eea76dd410543123be12267
Zaïnab <zeinab0695@gmail.com>
2023-12-19 15:53:40 +0100

    modifier

--- src/components/Places/ShowPlaces.jsx
+++ src/components/Places/ShowPlaces.jsx
@@ -107,11 +107,95 @@ function Show({ placeId }) {
     handleShow();
   }, [placeId]);
 
-  // ------------- AFFICHE LE LIEU -------------- //
+  const editPlace = async (placeId) => {
+    const formData = new FormData();
+    formData.append("title", place.title);
+    formData.append("street", place.street);
+    formData.append("postcode", place.postcode);
+    formData.append("description", place.description);
+    formData.append("city", place.city);
+    formData.append("description", place.street);
+    formData.append("file", place.file);
+
+    let options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: formData,
+    };
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/edit/${placeId}`,
+        options
+      );
+      if (!response.ok) {
+        console.log(response);
+
+        alert(`HTTP error! Status: ${response.status}`);
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
+  const handleSave = () => {
+    editPlace(value);
+    setIsEditing(false);
+  };
+
+  useEffect(() => {
+    handleShow();
+  }, [placeId]);
+
   const renderPlace = () => {
     return (
       <div>
-        <div className="info">
+        {isEditing ? (
+          <div>
+            <input
+              type="text"
+              value={place.title}
+              onChange={(e) => setPlace({ ...place, title: e.target.value })}
+            />
+            <input
+              type="text"
+              value={place.street}
+              onChange={(e) => setPlace({ ...place, street: e.target.value })}
+            />
+            <input
+              type="number"
+              value={place.postcode}
+              onChange={(e) => setPlace({ ...place, postcode: e.target.value })}
+            />
+            <input
+              type="text"
+              value={place.city}
+              onChange={(e) => setPlace({ ...place, city: e.target.value })}
+            />
+            <input type="file" onChange={(e) => handleFileChange(e)} />
+            <input
+              type="text"
+              value={place.description}
+              onChange={(e) =>
+                setPlace({ ...place, description: e.target.value })
+              }
+            />
+            <input
+              type="number"
+              value={place.name_category}
+              onChange={(e) =>
+                setPlace({ ...place, name_category: e.target.value })
+              }
+            />
+
+            <button onClick={handleSave}>Save</button>
+            <button onClick={() => setIsEditing(false)}>Annuler</button>
+          </div>
+        ) : (
           <ul>
             <li>
               <h1>{place.title}</h1>
@@ -121,23 +205,44 @@ function Show({ placeId }) {
               <img src={place.file}></img>
               <h2>{place.description}</h2>
               <h2>{place.name_category}</h2>
-              <button onClick={handleDelete}>Supprimer</button>
             </li>
+            <button onClick={() => setIsEditing(true)}>Modif</button>
           </ul>
-        </div>
-
-        <div className="emplacement">
-          {longitude != undefined || latitude != undefined ? (
-            <Leaflet latitude={latitude} longitude={longitude}></Leaflet>
-          ) : (
-            <div>erreur recuperation des données de localisation</div>
-          )}
-        </div>
-
-        <div className="reviews"></div>
+        )}
       </div>
     );
   };
+  // ------------- AFFICHE LE LIEU -------------- //
+  // const renderPlace = () => {
+  //   return (
+  //     <div>
+  //       <div className="info">
+  //         <ul>
+  //           <li>
+  //             <h1>{place.title}</h1>
+  //             <h2>{place.street}</h2>
+  //             <h2>{place.postcode}</h2>
+  //             <h2>{place.city}</h2>
+  //             <img src={place.file}></img>
+  //             <h2>{place.description}</h2>
+  //             <h2>{place.name_category}</h2>
+  //             <button onClick={handleDelete}>Supprimer</button>
+  //           </li>
+  //         </ul>
+  //       </div>
+
+  //       <div className="emplacement">
+  //         {longitude != undefined || latitude != undefined ? (
+  //           <Leaflet latitude={latitude} longitude={longitude}></Leaflet>
+  //         ) : (
+  //           <div>erreur recuperation des données de localisation</div>
+  //         )}
+  //       </div>
+
+  //       <div className="reviews"></div>
+  //     </div>
+  //   );
+  // };
 
   //  RENDRE LES DONNÉES VISIBLES PAR L'UTILISATEUR POUR LES REVIEWS
   const renderMyReview = () => {
